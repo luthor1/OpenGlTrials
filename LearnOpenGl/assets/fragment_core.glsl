92dd8f68ea05c66a15216aee35a4570ec092aef1
Ali Çuğun <105804209+luthor1@users.noreply.github.com>
2026-04-09 16:45:23 +0300

    Triangle

--- LearnOpenGl/assets/fragment_core.glsl
+++ LearnOpenGl/assets/fragment_core.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 out vec4 FragColor;
-uniform vec4 ourColor; // CPU’dan gelen renk
+
+in vec3 ourColor;
+
 void main()
 {
-    FragColor = ourColor;
+    FragColor = vec4(ourColor, 1.0);
 }
--- LearnOpenGl/src/main.cpp
+++ LearnOpenGl/src/main.cpp
@@ -1,38 +1,34 @@
-﻿#include <iostream>
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.h"
+
 #include <fstream>
 #include <sstream>
-#include <streambuf>
 #include <string>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void updateFPSCounter(GLFWwindow* window);
-std::string loadShaderSrc(const char* filePath);
-
-//  Global var for color
-float colorR = 0.0f;
-float colorG = 1.0f;
-float colorB = 0.0f;
 
 int main()
 {
-	int success;
-	char infoLog[512];
-
 	glfwInit();
 
 	// Set OpenGL version to 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL - Enhanced", NULL, NULL);
 
 	if (window == NULL)
-	{// Check if window was created
+	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -47,116 +43,61 @@ int main()
 	}
 
 	glViewport(0, 0, 800, 600);
-
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	/*
-		shaders
-	*/
+	// shader class initialization
+	Shader ourShader("assets/vertex_core.glsl", "assets/fragment_core.glsl");
 
-	// Compile vertex shader
-
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	std::string vertShaderSrc = loadShaderSrc("assets/vertex_core.glsl");
-	const GLchar* vertShader = vertShaderSrc.c_str();
-	glShaderSource(vertexShader, 1, &vertShader, NULL);
-	glCompileShader(vertexShader);
-
-	// catch error
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	// Compile fragment shader
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	std::string fragShaderSrc = loadShaderSrc("assets/fragment_core.glsl");
-	const GLchar* fragShader = fragShaderSrc.c_str();
-	glShaderSource(fragmentShader, 1, &fragShader, NULL);
-	glCompileShader(fragmentShader);
-
-	// catch error
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAG::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success)
-	{
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::ShaderProgram::LINKING_FAILED\n" << infoLog << std::endl;
-	}
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	// vertex array
-
+	// vertex array with colors (x, y, z, r, g, b)
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f ,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f // we can add more vertices here
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
 	};
 
-	// VAO, VBO
 	unsigned int VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
-	// bind VAO
 	glBindVertexArray(VAO);
 
-	// bind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// set attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
-	int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	while (!glfwWindowShouldClose(window))
 	{
-		// process input
 		processInput(window);
 
-		// rendering commands here
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw shapes
+		ourShader.use();
+
+		// Create transformation matrix
+		glm::mat4 trans = glm::mat4(1.0f); // identity matrix
+		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f)); // rotate around Z axis
+		// trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5)); // optional: scale it down
+
+		ourShader.setMat4("transform", trans);
+
 		glBindVertexArray(VAO);
-		glUseProgram(shaderProgram);
-
-		// set uniform color from global var
-		glUniform4f(vertexColorLocation, colorR, colorG, colorB, 1.0f);
-
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		// send new frame to window
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
-		// fps counter
 		updateFPSCounter(window);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
 	return 0;
@@ -171,18 +112,6 @@ void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
-
-	//  klavyeden renk değiştirme  ---
-	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
-		colorR = 1.0f; colorG = 0.0f; colorB = 0.0f; // red
-	}
-	if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS) {
-		colorR = 0.0f; colorG = 1.0f; colorB = 0.0f; //green
-	}
-	if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS) {
-		colorR = 0.0f; colorG = 0.0f; colorB = 1.0f; //blue
-	}
-	
 }
 
 void updateFPSCounter(GLFWwindow* window)
@@ -191,38 +120,11 @@ void updateFPSCounter(GLFWwindow* window)
 	static int frameCount = 0;
 	double currentTime = glfwGetTime();
 	frameCount++;
-	// If a second has passed.
 	if (currentTime - previousTime >= 1.0) {
-		// Display the frame count here any way you want.
 		std::ostringstream ss;
 		ss << "OpenGL FPS: " << frameCount;
 		glfwSetWindowTitle(window, ss.str().c_str());
 		frameCount = 0;
 		previousTime = currentTime;
 	}
-}
-
-
-std::string loadShaderSrc(const char* filePath)
-{
-	std::ifstream file;
-	std::stringstream buf;
-
-	std::string ret = "";
-
-	file.open(filePath);
-
-	if (file.is_open())
-	{
-		buf << file.rdbuf();
-		ret = buf.str();
-	}
-	else
-	{
-		std::cout << "Could not open: " << filePath << std::endl;
-	}
-
-	file.close();
-
-	return ret;
 }
